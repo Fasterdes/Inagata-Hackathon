db11c59c9edff4eb8a3a62346bd794d01486d399
= <keyn432des@gmail.com>
2022-05-25 10:49:38 +0700

    feat: init_nuxt_js

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  daisyui: {
+    themes: [{
+      mytheme: {
+
+        "primary": "#161616",
+
+        "secondary": "#1A1919",
+
+        "accent": "#262626",
+
+        "neutral": "#000000",
+
+        "base-100": "#F7F7F7",
+
+        "info": "#0072F5",
+
+        "success": "#21CA51",
+
+        "warning": "#FF6052",
+
+        "error": "#DE1B8D",
+      },
+    }, ],
+  },
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors : { 
+        pastel : { 
+          blue : { 
+            DEFAULT : "#7CBBE3",
+            'light-blue' : "#B1E1FF",
+          },
+          yellow : '#F9F0BA' , 
+          orange : '#FED28F' , 
+          purple : '#D1A4FF',
+          green : '#B6E2D0',
+        }
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
