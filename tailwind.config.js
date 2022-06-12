2668d08168a4438d15493bf04e11b1f64649f312
ferdiyansyah <17410100081@dinamika.ac.id>
2022-06-12 16:19:42 +0700

    adding assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,55 @@
 module.exports = {
   daisyui: {
-    themes: [{
-      mytheme: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#161616',
 
-        "primary": "#161616",
+          secondary: '#1A1919',
 
-        "secondary": "#1A1919",
+          accent: '#262626',
 
-        "accent": "#262626",
+          neutral: '#000000',
 
-        "neutral": "#000000",
+          'base-100': '#F7F7F7',
 
-        "base-100": "#F7F7F7",
+          info: '#0072F5',
 
-        "info": "#0072F5",
+          success: '#21CA51',
 
-        "success": "#21CA51",
+          warning: '#FF6052',
 
-        "warning": "#FF6052",
-
-        "error": "#DE1B8D",
+          error: '#DE1B8D',
+        },
       },
-    }, ],
+    ],
   },
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
   ],
   theme: {
     extend: {
-      colors : { 
-        pastel : { 
-          blue : { 
-            DEFAULT : "#7CBBE3",
-            'light-blue' : "#B1E1FF",
+      colors: {
+        pastel: {
+          blue: {
+            DEFAULT: '#7CBBE3',
+            'light-blue': '#B1E1FF',
           },
-          yellow : '#F9F0BA' , 
-          orange : '#FED28F' , 
-          purple : '#D1A4FF',
-          green : '#B6E2D0',
-        }
-      }
+          yellow: '#F9F0BA',
+          orange: '#FED28F',
+          purple: '#D1A4FF',
+          green: '#B6E2D0',
+        },
+      },
+    },
+
+    fontFamily: {
+      poppins: 'Poppins, sans-serif',
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 }
